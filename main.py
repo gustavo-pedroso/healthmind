79db1b42cf6a4aff257119f936fe560b2081b1f0
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-22 18:31:42 -0300

    created DHTSensor class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-import Adafruit_DHT
+import DHTSensor
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+    result = DHTSensor.read(4)
+    print(result)
