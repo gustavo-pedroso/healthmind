ea873c1c7d81d56c080aa3e73a8afdddb629a391
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-28 14:33:03 -0300

    testing sensor

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import dht_sensor
 import rele
 
 
-# sensor = dht_sensor.DHTSensor(4)
-# while True:
-#     result = sensor.read()
-#     print(result)
-
-
-rele = rele.Rele(17)
-
+sensor = dht_sensor.DHTSensor(17)
 while True:
-    a = input()
-    a = int(a)
-    if a == 0:
-        rele.off()
-    elif a == 1:
-        rele.on()
-    print(a)
+    result = sensor.read()
+    print(result)
+
+
+# rele = rele.Rele(17)
+#
+# while True:
+#     a = input()
+#     a = int(a)
+#     if a == 0:
+#         rele.off()
+#     elif a == 1:
+#         rele.on()
+#     print(a)
