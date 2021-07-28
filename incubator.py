06a406c6e5586b6a9f0305fd5da3052e816c5bb4
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-28 17:45:21 -0300

    added incubator class

---
+++ incubator.py
@@ -0,0 +1,22 @@
+from switch import Switch
+from dht_sensor import DHTSensor
+
+
+class Incubator:
+
+    def __init__(self, sensor_gpio_number, switch_gpio_number, target_temperature):
+        self.sensor = DHTSensor(sensor_gpio_number)
+        self.switch = Switch(switch_gpio_number)
+        self.target_temperature = target_temperature
+
+    def set_temperature(self, target_temperature):
+        self.target_temperature = target_temperature
+
+    def monitor(self):
+        temperature = self.sensor.read()['temperature']
+        print(f'current temperature: {temperature}°C')
+        if temperature < self.target_temperature:
+            self.switch.on()
+        elif temperature >= self.target_temperature:
+            self.switch.off()
+        print(f'heater state: {self.switch.get_state()}')
