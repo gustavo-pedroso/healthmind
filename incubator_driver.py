178e865a01cba3dc92d27a4c5746bbe901f40a1e
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-28 19:45:52 -0300

    ajdusted timings

--- incubator_driver.py
+++ incubator_driver.py
@@ -8,8 +8,8 @@ current_time = int(time.time())
 # create new Incubator with sensor, switch and target temperature
 incubator = Incubator(18, 23, 30)
 
-# run for 55s and exit, scheduled in cron for every minute
-while current_time - start_time < 55:
+# run for 10mins and exit, scheduled in cron for every 10mins
+while current_time - start_time < 590:
     incubator.monitor(sys.argv[1])
-    time.sleep(5)
+    time.sleep(10)
     current_time = int(time.time())
