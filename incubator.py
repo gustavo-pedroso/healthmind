3d552383b3cafb1d8366023cd26c1d7bd409a3ab
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-30 13:06:36 -0300

    testing incubator email feature

--- incubator.py
+++ incubator.py
@@ -1,19 +1,22 @@
 from switch import Switch
 from dht_sensor import DHTSensor
 from datetime import datetime
+from email_util import EmailUtil
 
 
 class Incubator:
 
-    def __init__(self, sensor_gpio, heater_gpio, target_temperature):
+    def __init__(self, sensor_gpio, heater_gpio, target_temperature, update_time, email_notify_hours):
         self.sensor = DHTSensor(sensor_gpio)
         self.heater = Switch(heater_gpio)
         self.target_temperature = target_temperature
+        self.email_notify_hours = email_notify_hours
+        self.update_time = update_time
 
     def set_temperature(self, target_temperature):
         self.target_temperature = target_temperature
 
-    def monitor(self, log_file=None):
+    def monitor(self, json_email_info, log_file=None):
         temperature = self.sensor.read()['temperature']
 
         # monitor temperature
@@ -21,6 +24,13 @@ class Incubator:
             self.heater.on()
         else:
             self.heater.off()
+
+        if json_email_info:
+            if datetime.now().hour in self.email_notify_hours:
+                self.email_notify_hours.remove(datetime.now().hour)
+                email = EmailUtil(json_email_info)
+                msg = f'Incubator Running OK\nTemperature: {temperature}\n'
+                email.send_email(msg)
 
         print(f'temperature: {temperature} / {self.target_temperature} | heater: {self.heater.get_state()}')
         if log_file:
--- incubator_driver.py
+++ incubator_driver.py
@@ -6,11 +6,11 @@ start_time = int(time.time())
 current_time = int(time.time())
 
 # create new Incubator with sensor, switch and target temperature
-incubator = Incubator(18, 23, 30)
+incubator = Incubator(18, 23, 30, 10, list(range(0, 24)))
 
 
 # run for 10mins and exit, scheduled in cron for every 10mins
-while current_time - start_time < 590:
-    incubator.monitor(sys.argv[1])
-    time.sleep(10)
+while current_time - start_time < 86400 - incubator.update_time:
+    incubator.monitor(sys.argv[1], sys.argv[2])
+    time.sleep(incubator.update_time)
     current_time = int(time.time())
