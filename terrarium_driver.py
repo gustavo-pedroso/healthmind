5e5970d4cdef7f66e522a9709b500005169a3f51
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-29 21:27:39 -0300

    testing all components

--- terrarium_driver.py
+++ terrarium_driver.py
@@ -7,6 +7,9 @@ start_time = int(time.time())
 current_time = int(time.time())
 
 terrarium = Terrarium(12, 25, 18, 24, 23, 30, 50)
+
+terrarium.lights.on()
+terrarium.fans.on()
 while current_time - start_time < 590:
     terrarium.monitor(sys.argv[1])
     time.sleep(10)
