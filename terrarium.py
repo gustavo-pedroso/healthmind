f91de41b946d6a5813b0b2a96147324287a5afc3
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-29 21:15:18 -0300

    added monitor method to Terrarium

--- terrarium.py
+++ terrarium.py
@@ -14,4 +14,29 @@ class Terrarium:
         self.target_temperature = target_temperature
         self.target_humidity = target_humidity
 
-    
+    def monitor(self, log_file=None):
+        sensor_data = self.sensor.read()
+        temperature = sensor_data['temperature']
+        humidity = sensor_data['humidity']
+
+        if temperature < self.target_temperature:
+            self.heaters.on()
+        else:
+            self.heaters.off()
+
+        if humidity < self.target_humidity:
+            self.humidifier.on()
+        else:
+            self.humidifier.off()
+
+        print(f'current temperature: {temperature}°C / target temperature {self.target_temperature}°C')
+        print(f'heaters state: {self.heaters.get_state()}')
+
+        print(f'current humidity: {humidity}% / target humidity {self.target_temperature}%')
+        print(f'humidifier state: {self.humidifier.get_state()}')
+
+        if log_file:
+            with open(log_file, 'a+') as f:
+                t = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+                f.write(f'{t} - {temperature} / {self.target_temperature} / {self.heaters.get_state()}\n')
+                f.write(f'{t} - {humidity} / {self.target_humidity} / {self.humidifier.get_state()}\n')
--- terrarium_driver.py
+++ terrarium_driver.py
@@ -1,12 +1,13 @@
 from terrarium import Terrarium
 import time
+import sys
 
+
+start_time = int(time.time())
+current_time = int(time.time())
 
 terrarium = Terrarium(12, 25, 18, 23, 24, 30, 50)
-
-terrarium.heaters.on()
-terrarium.fans.on()
-terrarium.lights.on()
-terrarium.humidifier.on()
-
-time.sleep(10)
+while current_time - start_time < 590:
+    terrarium.monitor(sys.argv[1])
+    time.sleep(10)
+    current_time = int(time.time())
