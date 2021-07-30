94ded9d8fb59878911cef96f1781c29ed71a4222
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-29 21:17:20 -0300

    fixed gpio order

--- terrarium_driver.py
+++ terrarium_driver.py
@@ -6,7 +6,7 @@ import sys
 start_time = int(time.time())
 current_time = int(time.time())
 
-terrarium = Terrarium(12, 25, 18, 23, 24, 30, 50)
+terrarium = Terrarium(12, 25, 18, 24, 23, 30, 50)
 while current_time - start_time < 590:
     terrarium.monitor(sys.argv[1])
     time.sleep(10)
