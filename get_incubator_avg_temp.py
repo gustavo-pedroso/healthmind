10a7a84301a8cc6552c39cdb1d84d21efb68334c
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-31 11:14:07 -0300

    improved formatting

--- get_incubator_avg_temp.py
+++ get_incubator_avg_temp.py
@@ -7,4 +7,4 @@ states = [t.split(' ')[9].replace('\n', '') for t in lines]
 print(f'mode temperature: {statistics.mode(temps)}°C')
 print(f'mean temperature: {statistics.mean(temps)}°C')
 print(f'median temperature: {statistics.median(temps)}°C')
-print(f"heater usage: {(round(states.count('ON')/len(states), 2)) * 100}%")
+print(f"heater usage: {(round(states.count('ON')/len(states), 4)) * 100}%")
