87bd4004c8e9c1f1dc2e824c68e403cc493abbf9
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-31 12:42:34 -0300

    added metric

--- get_incubator_avg_temp.py
+++ get_incubator_avg_temp.py
@@ -5,7 +5,7 @@ with open('/home/pi/Documents/healthmind/incubator_logs.txt') as log_file:
 temps = [float(t.split(' ')[4]) for t in lines]
 states = [t.split(' ')[9].replace('\n', '') for t in lines]
 print(f'mode temperature: {statistics.mode(temps)}°C')
-print(f'mean temperature: {statistics.mean(temps)}°C')
+print(f'mean temperature: {statistics.mean(temps):.2f}°C')
 print(f'median temperature: {statistics.median(temps)}°C')
 print(f"heater usage: {states.count('ON')/len(states) * 100:.2f}%")
 
