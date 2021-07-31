a2f6ab013d0408fd10c60675eac8abc49958c1a0
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-31 11:09:34 -0300

    removed \n

--- get_incubator_avg_temp.py
+++ get_incubator_avg_temp.py
@@ -3,7 +3,7 @@ import statistics
 with open('/home/pi/Documents/healthmind/incubator_logs.txt') as log_file:
     lines = log_file.readlines()
 temps = [float(t.split(' ')[4]) for t in lines]
-states = [t.split(' ')[9] for t in lines]
+states = [t.split(' ')[9].replace('\n', '') for t in lines]
 print(f'mode temperature: {statistics.mode(temps)}°C')
 print(f'mean temperature: {statistics.mean(temps)}°C')
 print(f'median temperature: {statistics.median(temps)}°C')
