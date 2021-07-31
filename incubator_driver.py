81d61698a26d20fd73801430ba0de302bf99dbf3
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-31 20:37:35 -0300

    testing higher refresh rate

--- incubator_driver.py
+++ incubator_driver.py
@@ -8,7 +8,7 @@ kill_previous_from_file("/home/pi/Documents/healthmind/incubator_last_pid.tmp")
 incubator = Incubator(sensor_gpio=21,
                       heater_gpio=20,
                       target_temperature=25,
-                      update_time=5,
+                      update_time=1,
                       email_notify_hours=list(range(0, 24)),
                       log_file='/home/pi/Documents/healthmind/incubator_logs.txt',
                       json_email_info='/home/pi/Documents/healthmind/incubator_email_info.json',
