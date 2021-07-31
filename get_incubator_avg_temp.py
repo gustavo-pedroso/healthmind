7607736109010a413004968243b462a9a91cbe26
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-07-30 22:16:50 -0300

    add helper file

---
+++ get_incubator_avg_temp.py
@@ -0,0 +1,8 @@
+import statistics
+
+with open('/home/pi/Documents/healthmind/incubator_logs.txt') as log_file:
+    lines = log_file.readlines()
+temps = [t.split(' ')[4] for t in lines]
+print(f'mode temperature: {statistics.mode(temps)}°C')
+print(f'mean temperature: {statistics.mean(temps)}°C')
+print(f'median temperature: {statistics.median(temps)}°C')
