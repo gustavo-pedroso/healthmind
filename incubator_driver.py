4a9012636749c5208991e8519e526cd601129ce9
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-08-04 13:08:19 -0300

    fixing hours

--- incubator_driver.py
+++ incubator_driver.py
@@ -9,7 +9,7 @@ incubator = Incubator(sensor_gpio=21,
                       heater_gpio=20,
                       target_temperature=25,
                       update_time=5,
-                      email_notify_hours=list(range(0, 23)),
+                      email_notify_hours=list(range(0, 24)),
                       log_file='/home/pi/Documents/healthmind/incubator_logs.txt',
                       json_email_info='/home/pi/Documents/healthmind/incubator_email_info.json',
                       json_api_info='/home/pi/Documents/healthmind/api_info.json')
