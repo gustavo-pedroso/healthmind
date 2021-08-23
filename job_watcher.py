48053e0dfef106575d52796183ab0393a0714d73
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-08-23 02:07:18 -0300

    update job watcher

--- job_watcher.py
+++ job_watcher.py
@@ -1,9 +1,11 @@
 import os
+import time
 
 
 class JobWatcher:
-    def __init__(self, job_names):
+    def __init__(self, job_names, files_names):
         self.job_names = job_names
+        self.file_names = files_names
 
     @staticmethod
     def reboot():
@@ -15,10 +17,16 @@ class JobWatcher:
             if name not in running:
                 self.reboot()
 
+        for name in self.file_names:
+            if int(time.time()) - int(os.path.getmtime(name)) > 30 * 60:
+                self.reboot()
+
 
 if __name__ == '__main__':
-    job_watcher = JobWatcher(['/bin/sh -c sleep 60 && python3 /home/pi/Documents/healthmind/incubator_driver.py',
+    job_watcher = JobWatcher(job_names=['/bin/sh -c sleep 60 && python3 /home/pi/Documents/healthmind/incubator_driver.py',
                               '/bin/sh -c sleep 60 && python3 /home/pi/Documents/healthmind/terrarium_driver.py',
                               'python3 /home/pi/Documents/healthmind/incubator_driver.py',
-                              'python3 /home/pi/Documents/healthmind/terrarium_driver.py'])
+                              'python3 /home/pi/Documents/healthmind/terrarium_driver.py'],
+                             files_names=['/home/pi/Documents/healthmind/incubator_logs.txt',
+                                          '/home/pi/Documents/healthmind/terrarium_logs.txt'])
     job_watcher.scan()
