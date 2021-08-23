87086ffee3579c6c41e2cba26088ec86d1150694
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-08-23 01:03:06 -0300

    added more error checking and reboot

--- dht_sensor.py
+++ dht_sensor.py
@@ -1,4 +1,5 @@
 import Adafruit_DHT
+import os
 
 
 class DHTSensor:
@@ -6,6 +7,8 @@ class DHTSensor:
     def __init__(self, gpio_number):
         self.gpio_number = gpio_number
         self.max_retry = 3
+        self.max_failure_count_reboot = 3
+        self.current_failures_in_a_row = 0
         self.last_valid = {'humidity': 50.0, 'temperature': 25}
 
         self.min_t = -2.0
@@ -19,7 +22,11 @@ class DHTSensor:
             humidity, temperature = Adafruit_DHT.read_retry(11, self.gpio_number)
             if self.min_t <= temperature <= self.max_t and self.min_h <= humidity <= self.max_h:
                 self.last_valid = {'humidity': humidity, 'temperature': temperature}
+                self.current_failures_in_a_row = 0
                 return {'humidity': humidity, 'temperature': temperature}
             print(f'sensor read invalid data: humidity: {humidity}, temperature: {temperature}')
             retry += 1
+        self.current_failures_in_a_row += 1
+        if self.current_failures_in_a_row == self.max_failure_count_reboot:
+            os.system('sudo reboot')
         return self.last_valid
