79f5859f2098cd8a20020b10b802c544bc7ef270
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-08-24 00:49:17 -0300

    improved job_watcher

--- job_watcher.py
+++ job_watcher.py
@@ -1,5 +1,6 @@
 import os
 import time
+from datetime import datetime
 
 
 class JobWatcher:
@@ -13,24 +14,23 @@ class JobWatcher:
 
     def scan(self):
         running = str(os.popen('ps -aux').read())
-        for name in self.job_names:
-            if name not in running:
+        for name in self.job_names.keys():
+            if running.count(name) < self.job_names[name]:
+                print(f'ops :| linux says we are missing some required processes. Rebooting :(')
                 self.reboot()
 
         for name in self.file_names:
-            if int(time.time()) - int(os.path.getmtime(name)) > 30 * 60:
-                print('bye bye stupid')
-                print(name)
-                print(int(time.time()) - int(os.path.getmtime(name)))
-                print(int(time.time()))
+            last_modified_date = int(os.path.getmtime(name))
+            now = int(time.time())
+            last_modified_date_ts = datetime.fromtimestamp(last_modified_date).strftime('%Y-%m-%d %H:%M:%S')
+            print(f'{name} was last modified at: {last_modified_date_ts}')
+            if now - last_modified_date > 60 * 30:
+                print(f'{name} was not updated in the last 30min, rebooting :/')
                 self.reboot()
 
 
 if __name__ == '__main__':
-    job_watcher = JobWatcher(job_names=['/bin/sh -c sleep 60 && python3 /home/pi/Documents/healthmind/incubator_driver.py',
-                              '/bin/sh -c sleep 60 && python3 /home/pi/Documents/healthmind/terrarium_driver.py',
-                              'python3 /home/pi/Documents/healthmind/incubator_driver.py',
-                              'python3 /home/pi/Documents/healthmind/terrarium_driver.py'],
+    job_watcher = JobWatcher(job_names={'terrarium_driver.py': 2, 'incubator_driver.py': 2},
                              files_names=['/home/pi/Documents/healthmind/incubator_logs.txt',
                                           '/home/pi/Documents/healthmind/terrarium_logs.txt'])
     job_watcher.scan()
