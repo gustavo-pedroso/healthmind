d9b7eb47fced368dffac924b99b9eee81732ae4b
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-08-30 22:24:40 -0300

    added retry to on off switch

--- switch.py
+++ switch.py
@@ -1,6 +1,7 @@
 import os
 from project_utils import log_reboot_causes
 from gpiozero import LED
+import time
 
 
 class Switch:
@@ -11,13 +12,25 @@ class Switch:
         self.switch.off()
 
     def on(self):
+        retry = 0
         self.switch.on()
+        while self.get_state() != 'ON' and retry < 3:
+            self.switch.on()
+            retry += 1
+            time.sleep(0.5)
+
         if self.get_state() != 'ON':
             log_reboot_causes('get_state != ON after setting to ON')
             os.system('sudo reboot')
 
     def off(self):
+        retry = 0
         self.switch.off()
+        while self.get_state() != 'OFF' and retry < 3:
+            self.switch.off()
+            retry += 1
+            time.sleep(0.5)
+
         if self.get_state() != 'OFF':
             log_reboot_causes('get_state != OFF after setting to OFF')
             os.system('sudo reboot')
