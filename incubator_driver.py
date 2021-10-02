b3f19df40f7e0730799089b0a42494c939498b46
Gustavo Pedroso <gustavopedroso1995@gmail.com>
2021-10-01 22:23:53 -0300

    starting incubator again :)

--- incubator_driver.py
+++ incubator_driver.py
@@ -1,11 +1,11 @@
 from incubator import Incubator
 
 # create new Incubator with sensor, switch and target temperature
-incubator = Incubator(sensor_gpio=[21, 26],
-                      heater_gpio=20,
+incubator = Incubator(sensor_gpio=[23, 24],
+                      heater_gpio=18,
                       target_temperature=27.0,
                       update_time=5,
-                      email_notify_hours=list(range(0, 24)),
+                      email_notify_hours=list(range(0, 24, 2)),
                       log_file='/home/pi/Documents/healthmind/incubator_logs.txt',
                       json_email_info='/home/pi/Documents/healthmind/incubator_email_info.json',
                       json_api_info='/home/pi/Documents/healthmind/api_info.json')
